32b979aa4807202dd686e196926f0c670980be0c
vagrant <vagrant@lighthouse-labs>
2016-08-09 20:55:21 +0000

    Add sign-in/sign-out

--- server/app/controllers/sessions_controller.rb
+++ server/app/controllers/sessions_controller.rb
@@ -7,16 +7,23 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_or_create_from_auth_hash(auth_hash)
     session[:user_id] = @user.id
+    session[:access_token] = @user.token
+    # Set the session access_token here - will have to figure out what to do when the token expires
     self.current_user
+    # Populating Kala's public google calendar will likely go here
 
-    
     #Will change this to go to react
-    redirect_to 'http://localhost:3000' , :notice => 'Signed in!'
+    redirect_to root_url , :notice => 'Signed in!'
   end
 
   def destroy
-    reset_session
+    reset_session # TODO implement this function, delete session[:user_id] and session[:access_token]
     redirect_to root_url, :notice => 'Signed out!'
+  end
+
+  def reset_session
+    session.delete(:user_id)
+    session.delete(:access_token)
   end
 
   def failure
