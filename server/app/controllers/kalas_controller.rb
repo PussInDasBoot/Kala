7c29504604448be2a15424604b1ca03642a1e249
jblang94 <jbarlang@gmail.com>
2016-08-11 20:58:33 +0000

    Add route for front-end to test rendering a user's google calendar events

--- server/app/controllers/kalas_controller.rb
+++ server/app/controllers/kalas_controller.rb
@@ -3,6 +3,21 @@ class KalasController < ApplicationController
   end
 
   def get_user_events
-    
+    service = Google::Apis::CalendarV3::CalendarService.new
+    service.authorization = session[:access_token]
+    current_user_calendar_id = current_user.email
+    ## GET CURRENT USER'S EVENTS ###
+    response = service.list_events(current_user_calendar_id,
+                                   max_results: 3,
+                                   single_events: true,
+                                   order_by: 'startTime',
+                                   time_min: DateTime.new(2016,8,3,0,0,0,Rational(-7,24)).to_s,
+                                   time_max: DateTime.new(2016,8,7,0,0,0,Rational(-7,24)).to_s)
+    # response.items.each do |event|
+    #   start = event.start.date || event.start.date_time
+    #   puts "- #{event.summary} (#{start}) #{event.location}"
+    #   puts "\n\n"
+    # end
+    render json: response.items
   end
 end
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get '/logout', to: 'sessions#destroy', as: 'logout'
   get '/login', to: 'sessions#new', as: 'login'
   
+  get '/get_user_events', to: 'kalas#get_user_events'
+  
   resources :studios, only: [:index]
   resource :kala, only: [:show]
   root 'kalas#show'
