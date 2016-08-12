54a87fbe46a66d9f0ae5236dbaf7c61f8b411185
jblang94 <jbarlang@gmail.com>
2016-08-12 21:29:21 +0000

    Get user busy time

--- server/app/controllers/kalas_controller.rb
+++ server/app/controllers/kalas_controller.rb
@@ -27,4 +27,37 @@ class KalasController < ApplicationController
     end
     render json: events
   end
+
+  def get_classes_outside_busy_time
+    # Time.zone = "America/Vancouver"
+    # start_time = Time.zone.now
+    # end_time = time_min + 1.week
+    # yoga_classes = YogaClass.where("start_time >= ? AND end_time <= ?", start_time, end_time)
+    # busy_times = get_busy_times(current_user.email, session[:access_token])
+
+    # busy_times.each do |busy_time|
+    #   yoga_classes = yoga_classes.where("start_time <= ? OR end_time >= ?", )
+    # end
+    render json: get_busy_times(current_user.email, session[:access_token])
+  end
+
+  def get_busy_times(calendar_id, access_token) 
+    Time.zone = "America/Vancouver"
+    time_min = Time.zone.now.to_datetime
+    time_max = (time_min + 1.week).to_datetime
+
+    free_busy_request_item = Google::Apis::CalendarV3::FreeBusyRequestItem.new
+    free_busy_request_item.id = calendar_id
+
+    free_busy_request = Google::Apis::CalendarV3::FreeBusyRequest.new
+    free_busy_request.time_zone = "America/Vancouver"
+    free_busy_request.time_min = time_min
+    free_busy_request.time_max = time_max 
+    free_busy_request.items = [free_busy_request_item]
+
+    service = Google::Apis::CalendarV3::CalendarService.new
+    service.authorization = access_token
+    free_busy_response = service.query_freebusy(free_busy_request)
+    free_busy_response.calendars[calendar_id].busy
+  end
 end
