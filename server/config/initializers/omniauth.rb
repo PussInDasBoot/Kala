54c0b96255040b76814b8fcd120319f9646360b9
jblang94 <jbarlang@gmail.com>
2016-08-13 22:10:54 +0000

    Handle the case when a user denies access to Google account

--- server/config/initializers/omniauth.rb
+++ server/config/initializers/omniauth.rb
@@ -10,3 +10,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
            scope: scopes.join(" "), jwt_leeway: 600
 end
+
+OmniAuth.config.on_failure = SessionsController.action(:failure)
