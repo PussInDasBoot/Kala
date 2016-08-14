cc047447772ae861ae4eff093c9150033cdd74a1
yemelyn <pommy.shortcake@gmail.com>
2016-08-14 19:25:57 +0000

    Fixed moment formatting

--- client/src/Day.jsx
+++ client/src/Day.jsx
@@ -5,27 +5,35 @@ import moment from 'moment';
 var Day = React.createClass({
   freeEventsFinder: function (event_data) {
     var schedule = [];
-    var start = '6:00 am';
-    var end = '21:00 pm';
+    
     for(var i=0, l=event_data.length; i<l; i++){
-        end = event_data[i].start;
+      var startOfDay = moment(event_data[i]).startOf('day');
+      var sixAM = startOfDay.add(6, 'hours');
+      var start = moment(sixAM).format('YYYY-MM-DDTHH:mm:ss.SSSSZ');
+      var endOfDay = moment(event_data[i]).endOf('day');
+      var ninePM = endOfDay.subtract(3, 'hours').add(1, 'minute');
+      var convertedNinePM = moment(ninePM).format('YYYY-MM-DDTHH:mm:ss.SSSSZ');
+      console.log("converted", convertedNinePM);
+      var end = event_data[i].start;
         
-        if(i)
-            start = event_data[i-1].end;   
-        
-        if((end && !i) || (end && i && moment(event_data[i-1].end).format('HH:mm') < moment(event_data[i].start).format('HH:mm')))
-            schedule.push({summary: 'Free Time', start: start, end: end});
-                
-        schedule.push(event_data[i]);
-        
-        if(i+1 === l){
-            start = event_data[i].end;
-            
-            if(start)
-                schedule.push({summary: 'Free Time', start: start, end: '21:00 pm'});
-        }
+      if(i)
+          start = event_data[i-1].end;   
+      
+      if((end && !i) || (end && i && moment(event_data[i-1].end).format('HH:mm') < moment(event_data[i].start).format('HH:mm')))
+          schedule.push({summary: 'Free Time', start: start, end: end});
+              
+      schedule.push(event_data[i]);
+      
+      if(i+1 === l){
+          start = event_data[i].end;
+          
+          if(start)
+              schedule.push({summary: 'Free Time', start: start, end: convertedNinePM});
+      }
     }
+    console.log("schedule", schedule);
     return schedule;
+
   },
   
   render() {
