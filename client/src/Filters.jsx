f194d37fa3f393933ba907be2431304b4c5f786d
yemelyn <pommy.shortcake@gmail.com>
2016-08-15 21:55:30 +0000

    Filters are working

--- client/src/Filters.jsx
+++ client/src/Filters.jsx
@@ -4,57 +4,74 @@ import React, {Component} from 'react';
 
 var Filters = React.createClass({
   getInitialState: function (){
-    return {commitment: []}
+    return {commitment: "", price: "", class_name: "", studio_name: "", location: ""}
   },
   componentDidMount: function() {
-    $(document).ready(function() {
-        $('select').material_select();
-    });
+    $('select').on("change", () => {this.handleChange()}).material_select();
   },
   handleChange(e) {
-    this.setState({commitment: e.target.value}, function (){
-      console.log(this.state)
-    })
+    var commitmentvalue = (this.commitments.find(function (input){
+        return input.checked;
+      }) || {}).value
+    this.setState({
+      commitment: commitmentvalue,
+      max_price: commitmentvalue && this.max_price.value,
+      class_name: this.class_name.value,
+      studio_name: this.studio_name.value,
+      location: this.location.value,
+      rating: (this.ratings.find(function (input){
+        return input.checked;
+      }) || {}).value
+    }, function (){
+      console.log(this.state);
+    }.bind(this));
+
+  },
+  handleFormSubmit (e) {
+    e.preventDefault();
+
+    this.props.onFilterSubmit(this.state);
   },
   render() {
+    this.commitments = [];
+    this.ratings = [];
     return (
-      <form action="//localhost:3001/classes_outside_busy_time" id="filter-classes">
+      <form onSubmit={this.handleFormSubmit} id="filter-classes">
         <div className="row">
           <div className="col s6">
             <label>Choose a level of commitment:</label>
             <p>
-              <input name="commitment" value="single" type="radio" id="commitment1" onClick={this.handleChange}/>
+              <input ref={(el) => {this.commitments[0] = el;}} name="commitment" value="single" type="radio" id="commitment1" onClick={this.handleChange}/>
               <label htmlFor="commitment1">Single</label>
             </p>
             <p>
-              <input name="commitment" value="pass" type="radio" id="commitment2" />
+              <input ref={(el) => {this.commitments[1] = el;}} name="commitment" value="pass" type="radio" id="commitment2" />
               <label htmlFor="commitment2" onClick={this.handleChange}>Pass</label>
             </p>
             <p>
-              <input name="commitment" value="membership" type="radio" id="commitment3" />
+              <input ref={(el) => {this.commitments[2] = el;}} name="commitment" value="membership" type="radio" id="commitment3" />
               <label htmlFor="commitment3" onClick={this.handleChange}>Membership</label>
             </p>
           </div>
           
           <div className="col s6">
             <p className="range-field">
-            {this.state.commitment.length >0 &&
-              <span>
+              <span style={{visibility: this.state.commitment ? "visible" : "hidden"}}>
             <label>Maximum Price</label>
-              <input type="range" id="price-slider" min="0" max="50" name="max_price"/>
+              <input ref={(el) => {this.max_price = el}} type="range" id="price-slider" min="5" max="30" name="max_price" onChange={this.handleChange}/>
               </span>
-              }
             </p>
           </div>
           <div className="input-field col s6">
-            <input name="class_name" id="class-name" type="text" className="validate" placeholder="Class Name"/>
+            <input ref={(el) => {this.class_name = el}} name="class_name" id="class-name" type="text" className="validate" placeholder="Class Name" onChange={this.handleChange}/>
             <label htmlFor="class-name">Class Name</label>
           </div>
 
           <div className="input-field col s6">
-            <select name="studio_name" form="filter-classes">
+            <select ref={(el) => {this.studio_name = el}} name="studio_name" form="filter-classes">
             <option value=""></option>
-            { this.props.studios.map(function(studio) {
+
+            { this.props.studios.map((studio) => {
                return <option key={studio.id} value={studio.name}>{studio.name}</option>
              })}
             </select>
@@ -62,7 +79,7 @@ var Filters = React.createClass({
           </div>
 
           <div className="input-field col s6">
-            <select name="location" form="filter-classes">
+            <select ref={(el) => {this.location = el}} name="location" form="filter-classes" onChange={this.handleChange}>
               <option value=""></option>
               <option value="Downtown Vancouver">Downtown Vancouver</option>
               <option value="Kitsilano">Kitsilano</option>
@@ -75,19 +92,19 @@ var Filters = React.createClass({
           <div className="col s6">
             <label>Minimum Rating</label>
               <p>
-              <input name="rating" value="1" type="radio" id="rating1" />
+              <input ref={(el) => {this.ratings[0] = el;}} name="rating" value="1" type="radio" id="rating1" />
               <label className="radio" htmlFor="rating1">1</label>
 
-              <input name="rating" value="2" type="radio" id="rating2" />
+              <input ref={(el) => {this.ratings[1] = el;}} name="rating" value="2" type="radio" id="rating2" />
               <label className="radio" htmlFor="rating2">2</label>
 
-              <input name="rating" value="3" type="radio" id="rating3" />
+              <input ref={(el) => {this.ratings[2] = el;}} name="rating" value="3" type="radio" id="rating3" />
               <label className="radio" htmlFor="rating3">3</label>
 
-              <input name="rating" value="4" type="radio" id="rating4" />
+              <input ref={(el) => {this.ratings[3] = el;}} name="rating" value="4" type="radio" id="rating4" />
               <label className="radio" htmlFor="rating4">4</label>
 
-              <input name="rating" value="5" type="radio" id="rating5" />
+              <input ref={(el) => {this.ratings[4] = el;}} name="rating" value="5" type="radio" id="rating5" />
               <label className="radio" htmlFor="rating5">5</label>
              </p>
           </div>
