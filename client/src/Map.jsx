d1a8e79c8bc5cf5ab248473d213577eeb46ed008
jblang94 <jbarlang@gmail.com>
2016-08-16 17:09:31 +0000

    Remove debug statement from Map.jsx

--- client/src/Map.jsx
+++ client/src/Map.jsx
@@ -19,7 +19,6 @@ var SimpleMap = React.createClass({
               defaultCenter={{ lat: 49.262852, lng: -123.116510 }}
               >
               { this.props.studios.map(function(studio) {
-                console.log(studio.name, studio.lat, studio.lng);
                 return <Marker key={studio.id} studio={studio}
                 position={{ lat: studio.lat, lng: studio.lng }}
                 studioid={{ studioid: studio.id }}
