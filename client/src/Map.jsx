ffa1afa6cb8b166024f9512d01255e101ede28ad
jblang94 <jbarlang@gmail.com>
2016-08-16 06:03:34 +0000

    Show studio name when hovering over marker

--- client/src/Map.jsx
+++ client/src/Map.jsx
@@ -19,9 +19,11 @@ var SimpleMap = React.createClass({
               defaultCenter={{ lat: 49.262852, lng: -123.116510 }}
               >
               { this.props.studios.map(function(studio) {
+                console.log(studio.name, studio.lat, studio.lng);
                 return <Marker key={studio.id} studio={studio}
                 position={{ lat: studio.lat, lng: studio.lng }}
-                studioid={{ studioid: studio.id }} />
+                studioid={{ studioid: studio.id }}
+                title={studio.name} />
               })}
             </GoogleMap>
           }
