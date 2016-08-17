6c1124f346e5d67d9b7e057a52f4eaaa06506baf
yemelyn <pommy.shortcake@gmail.com>
2016-08-17 00:20:46 +0000

    Added collapsible filters

--- client/src/Filters.jsx
+++ client/src/Filters.jsx
@@ -9,6 +9,9 @@ var Filters = React.createClass({
   componentDidMount: function() {
     $('select').on("change", () => {this.handleChange()}).material_select();
     $('.tooltipped').tooltip({delay: 50});
+    $('.collapsible').collapsible({
+      // accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
   },
   handleChange(e) {
     var commitmentvalue = (this.commitments.find(function (input){
@@ -36,8 +39,13 @@ var Filters = React.createClass({
     $("#max-price").css('visibility', 'hidden');
     this.setState({max_price: undefined});
     this.props.onFilterSubmit(this.state);
-    Materialize.toast('Filtered!', 4000, 'rounded toast') // 4000 is the duration of the toast
-  },
+    Materialize.toast('Filtered!', 4000, 'rounded toast'); // 4000 is the duration of the toast
+    $(".collapsible-header").removeClass(function(){
+    return "active";
+    });
+    $(".collapsible").collapsible({accordion: true});
+    $(".collapsible").collapsible({accordion: false});
+    },
   render() {
     this.commitments = [];
     this.ratings = [];
@@ -45,85 +53,86 @@ var Filters = React.createClass({
     var passToolTip = "Filter by the average per class price for class passes that include 5 to 25 sessions.";
     var membershipToolTip = "Filter by the average per class price for memberships based on twice per week frequency.";
     return (
-      <form onSubmit={this.handleFormSubmit} id="filter-classes">
-        <div className="row">
-          <div className="col s12 m6">
-            <label>Choose a level of commitment:</label>
-            <p>
-              <a className="tooltipped" data-position="right" data-delay="50" data-customcss="yellow" data-tooltip={singleToolTip}>
-                <input ref={(el) => {this.commitments[0] = el;}} name="commitment" value="single" type="radio" id="commitment1" onClick={this.handleChange}/>
-                <label htmlFor="commitment1">Single</label>
-              </a>
-            </p>
-            <p>
-              <a className="tooltipped" data-position="right" data-delay="50" data-tooltip={passToolTip}>
-                <input ref={(el) => {this.commitments[1] = el;}} name="commitment" value="pass" type="radio" id="commitment2" onClick={this.handleChange}/>
-                <label htmlFor="commitment2">Pass</label>
-              </a>
-            </p>
-            <p>
-              <a className="tooltipped" data-position="right" data-delay="50" data-tooltip={membershipToolTip}>
-                <input ref={(el) => {this.commitments[2] = el;}} name="commitment" value="membership" type="radio" id="commitment3" onClick={this.handleChange}/>
-                <label htmlFor="commitment3">Membership</label>
-              </a>
-            </p>
-          </div>
-          
-          <div className="col s12 m6">
-            <p className="range-field">
-              <span id="max-price"style={{visibility: this.state.commitment ? "visible" : "hidden"}}>
-            <label>Maximum Price</label>
-              <input ref={(el) => {this.max_price = el}} type="range" id="price-slider" min="5" max="30" name="max_price" onChange={this.handleChange}/>
-              </span>
-            </p>
-          </div>
-          <div className="input-field col s12 m6">
-            <input ref={(el) => {this.class_name = el}} name="class_name" id="class-name" type="text" className="validate" placeholder="Class Name" onChange={this.handleChange}/>
-            <label htmlFor="class-name">Class Name</label>
-          </div>
+      <ul className="collapsible" data-collapsible="accordion">
+        <li>
+          <div className="collapsible-header">Filters</div>
+          <div className="collapsible-body">
+            <form onSubmit={this.handleFormSubmit} id="filter-classes">
+              <div className="row">
+                <div className="col s12 m6">
+                  <label>Choose a level of commitment:</label><br />
+                    <a className="tooltipped" data-position="right" data-delay="50" data-customcss="yellow" data-tooltip={singleToolTip}>
+                      <input ref={(el) => {this.commitments[0] = el;}} name="commitment" value="single" type="radio" id="commitment1" onClick={this.handleChange}/>
+                      <label htmlFor="commitment1">Single</label>
+                    </a><br />
+                    <a className="tooltipped" data-position="right" data-delay="50" data-tooltip={passToolTip}>
+                      <input ref={(el) => {this.commitments[1] = el;}} name="commitment" value="pass" type="radio" id="commitment2" onClick={this.handleChange}/>
+                      <label htmlFor="commitment2">Pass</label>
+                    </a><br />
+                    <a className="tooltipped" data-position="right" data-delay="50" data-tooltip={membershipToolTip}>
+                      <input ref={(el) => {this.commitments[2] = el;}} name="commitment" value="membership" type="radio" id="commitment3" onClick={this.handleChange}/>
+                      <label htmlFor="commitment3">Membership</label>
+                    </a><br />
+                </div>
+                
+                <div className="col s12 m6">
+                  <p className="range-field">
+                    <span id="max-price"style={{visibility: this.state.commitment ? "visible" : "hidden"}}>
+                  <label>Maximum Price</label>
+                    <input ref={(el) => {this.max_price = el}} type="range" id="price-slider" min="5" max="30" name="max_price" onChange={this.handleChange}/>
+                    </span>
+                  </p>
+                </div>
+                <div className="input-field col s12 m6">
+                  <input ref={(el) => {this.class_name = el}} name="class_name" id="class-name" type="text" className="validate" placeholder="Class Name" onChange={this.handleChange}/>
+                  <label htmlFor="class-name">Class Name</label>
+                </div>
 
-          <div className="input-field col s12 m6">
-            <select ref={(el) => {this.studio_name = el}} name="studio_name" form="filter-classes">
-            <option value=""></option>
+                <div className="input-field col s12 m6">
+                  <select ref={(el) => {this.studio_name = el}} name="studio_name" form="filter-classes">
+                  <option value=""></option>
 
-            { this.props.studios.map((studio) => {
-               return <option key={studio.id} value={studio.name}>{studio.name}</option>
-             })}
-            </select>
-            <label>Select Studio</label>
-          </div>
+                  { this.props.studios.map((studio) => {
+                     return <option key={studio.id} value={studio.name}>{studio.name}</option>
+                   })}
+                  </select>
+                  <label>Select Studio</label>
+                </div>
 
-          <div className="input-field col s12 m6">
-            <select ref={(el) => {this.location = el}} name="location" form="filter-classes" onChange={this.handleChange}>
-              <option value=""></option>
-              <option value="Downtown Vancouver">Downtown Vancouver</option>
-              <option value="Kitsilano">Kitsilano</option>
-              <option value="Mount Pleasant">Mount Pleasant</option>
-              <option value='East Vancouver'>East Vancouver</option>
-            </select>
-            <label>Select Location</label>
-          </div>
+                <div className="input-field col s12 m6">
+                  <select ref={(el) => {this.location = el}} name="location" form="filter-classes" onChange={this.handleChange}>
+                    <option value=""></option>
+                    <option value="Downtown Vancouver">Downtown Vancouver</option>
+                    <option value="Kitsilano">Kitsilano</option>
+                    <option value="Mount Pleasant">Mount Pleasant</option>
+                    <option value='East Vancouver'>East Vancouver</option>
+                  </select>
+                  <label>Select Location</label>
+                </div>
 
-          <div className="col s12 m6">
-            <label>Minimum Rating</label>
-              <p>
-              <input ref={(el) => {this.ratings[0] = el;}} onClick={this.handleChange} name="rating" value="1" type="radio" id="rating1" />
-              <label className="radio" htmlFor="rating1">1</label>
+                <div className="col s12 m6">
+                  <label>Minimum Rating</label>
+                    <p>
+                    <input ref={(el) => {this.ratings[0] = el;}} onClick={this.handleChange} name="rating" value="1" type="radio" id="rating1" />
+                    <label className="radio" htmlFor="rating1">1</label>
 
-              <input ref={(el) => {this.ratings[1] = el;}} onClick={this.handleChange} name="rating" value="2" type="radio" id="rating2" />
-              <label className="radio" htmlFor="rating2">2</label>
+                    <input ref={(el) => {this.ratings[1] = el;}} onClick={this.handleChange} name="rating" value="2" type="radio" id="rating2" />
+                    <label className="radio" htmlFor="rating2">2</label>
 
-              <input ref={(el) => {this.ratings[2] = el;}} onClick={this.handleChange} name="rating" value="3" type="radio" id="rating3" />
-              <label className="radio" htmlFor="rating3">3</label>
+                    <input ref={(el) => {this.ratings[2] = el;}} onClick={this.handleChange} name="rating" value="3" type="radio" id="rating3" />
+                    <label className="radio" htmlFor="rating3">3</label>
 
-              <input ref={(el) => {this.ratings[3] = el;}} onClick={this.handleChange} name="rating" value="4" type="radio" id="rating4" />
-              <label className="radio" htmlFor="rating4">4</label>
-             </p>
-          </div>
-          
-          <input id="form-btn-text" className="waves-effect waves-light btn-flat form-btn btn" type="submit" label="Filter" onclick="Materialize.toast('Filtered!', 4000, 'rounded toast')"/>
-        </div>
-     </form>
+                    <input ref={(el) => {this.ratings[3] = el;}} onClick={this.handleChange} name="rating" value="4" type="radio" id="rating4" />
+                    <label className="radio" htmlFor="rating4">4</label>
+                   </p>
+                </div>
+                
+                <input id="form-btn-text" className="waves-effect waves-light btn-flat form-btn btn" type="submit" label="Filter" onclick="Materialize.toast('Filtered!', 4000, 'rounded toast')"/>
+              </div>
+           </form>
+         </div>
+       </li>  
+     </ul>
     );
     // Click event handler on the button that does ajax request for classes: "http://localhost:3001/classes_outside_busy_time" And changes parent state (App) state: classes with the new data.
   }
